78409e68e6596268a112664e07c2ba030ca42ae3
Levi Wright <lawright379@gmail.com>
2024-08-23 12:20:52 -0300

    added shop page header icon

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,12 +1,18 @@
 import { ShopContainer } from "./shop.styles";
+
 import Card from "../../components/card/card.component";
 import { CARD_TYPE_CLASSES } from "../../components/card/card.component";
+
+import Header from "../../components/header/header.component";
+import { HEADER_TYPE_CLASSES } from "../../components/header/header.component";
+
+import ShoppingBag from "../../assets/shopping-bag.component";
 
 const Shop = () => {
     return (
         <ShopContainer>
             <Card cardType={CARD_TYPE_CLASSES.shop}>
-                <h1>Shop</h1>
+                <Header headerType={HEADER_TYPE_CLASSES.primary}><ShoppingBag/>Shop</Header>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam quibusdam non, error aliquid odio impedit eaque, eius dicta, exercitationem animi ex incidunt nisi temporibus. Aut cupiditate temporibus accusamus quibusdam ullam?</p>
             </Card>
         </ShopContainer>
