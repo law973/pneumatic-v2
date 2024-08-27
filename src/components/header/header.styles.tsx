8227af3f89d6ffbb2fe389aa89743eb5742fb483
Levi Wright <lawright379@gmail.com>
2024-08-27 17:01:33 -0300

    added media queries and mobile responsiveness for font and svg sizing of the primary header in the header component

--- src/components/header/header.styles.tsx
+++ src/components/header/header.styles.tsx
@@ -5,8 +5,97 @@ export const PrimaryHeader = styled.h1`
     align-items: center;
 
     svg {
-        height: 24px;
-        width: 24px;
+        height: 27px;
+        width: 27px;
+    }
+
+    /* Portrait Breakpoints -------------------- */
+
+    /* Breakpoint (Small) */
+    @media screen and (min-width: 576px) and (orientation: portrait) {
+        font-size: 3.72rem;
+
+        svg {
+            height: 50px;
+            width: 50px;
+        }
+    }
+
+    /* Breakpoint (Medium) */
+    @media screen and (min-width: 768px) and (orientation: portrait) {
+        font-size: 4.25rem;
+
+        svg {
+            height: 56px;
+            width: 56px;
+        }
+    }
+
+    /* Breakpoint (Large) */
+    @media screen and (min-width: 992px) and (orientation: portrait) {
+        font-size: 5.84rem;
+
+        svg {
+            height: 78px;
+            width: 78px;
+        }
+    }
+
+    /* Breakpoint (Extra Large) and (orientation: portrait) */
+    /* @media screen and (min-width: 1200px) {
+        font-size:
+    } */
+
+    /* Breakpoint (Extra Extra Large) and (orientation: portrait) */
+    /* @media screen and (min-width: 1400px) {
+        font-size:
+    } */
+
+     /* Landscape Breakpoints -------------------- */
+
+    /* Landscape Breakpoint (Small) */
+    @media screen and (min-width: 576px) and (orientation: landscape) {
+        font-size: 2.13rem;
+    }
+
+    /* Landscape Breakpoint (Medium) */
+    @media screen and (min-width: 768px) and (orientation: landscape) {
+        font-size: 2.55rem;
+
+        svg {
+            height: 32px;
+            width: 32px;
+        }
+    }
+
+    /* Landscape Breakpoint (Large) */
+    @media screen and (min-width: 992px) and (orientation: landscape) {
+        font-size: 3.72rem;
+
+        svg {
+            height: 50px;
+            width: 50px;
+        }
+    }
+
+    /* Landscape Breakpoint (Extra Large) */
+    @media screen and (min-width: 1200px) and (orientation: landscape) {
+        font-size: 4.78rem;
+
+        svg {
+            height: 64px;
+            width: 64px;
+        }
+    }
+
+    /* Landscape Breakpoint (Extra Extra Large) */
+    @media screen and (min-width: 1400px) and (orientation: landscape) {
+        font-size: 5.31rem;
+
+        svg {
+            height: 70px;
+            width: 70px;
+        }
     }
 `;
 
