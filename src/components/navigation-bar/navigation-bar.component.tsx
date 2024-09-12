f773e147e545e6c43eebb569297eff970505b320
Levi Wright <lawright379@gmail.com>
2024-09-12 11:55:15 -0300

    navbar logo now links to home page, navbar appears or disappears upon user scrolling, navbar links move downwards when in an active state

--- src/components/navigation-bar/navigation-bar.component.tsx
+++ src/components/navigation-bar/navigation-bar.component.tsx
@@ -5,15 +5,46 @@ import {
     NavigationBarLinksContainer
 } from "./navigation-bar.styles";
 
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import HamburgerButton from "../hamburger-button/hamburger-button.component";
 
 const NavigationBar = () => {
+    // State variables to manage scroll behavior
+    const [previousScrollPosition, setPreviousScrollPosition] = useState(window.scrollY);
+
+    const [isScrollingUp, setIsScrollingUp] = useState(false);
+
+    useEffect(() => {
+        // Function to handle scroll events
+        const handleScroll = () => {
+            const currentScrollPosition = window.scrollY;
+            if (previousScrollPosition > currentScrollPosition) {
+                // Show navbar
+                setIsScrollingUp(true); 
+            } else {
+                // Hide navbar
+                setIsScrollingUp(false);
+            }
+            setPreviousScrollPosition(currentScrollPosition);
+        };
+
+        // Add scroll event listener when the component mounts
+        window.addEventListener('scroll', handleScroll);
+        
+        // Clean up by removing the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [previousScrollPosition]);
+
     return (
-        <NavigationBarContainer>
-            <SmallLogoContainer>P</SmallLogoContainer>
-            <LargeLogoContainer>Pneumatic</LargeLogoContainer>
+        <NavigationBarContainer
+            className={isScrollingUp ? 'active' : ''}
+        >
+            <SmallLogoContainer to='/'>P</SmallLogoContainer>
+            <LargeLogoContainer to='/'>Pneumatic</LargeLogoContainer>
 
             <HamburgerButton />
 
@@ -27,6 +58,6 @@ const NavigationBar = () => {
             </NavigationBarLinksContainer>
         </NavigationBarContainer>
     );
-}
+};
 
 export default NavigationBar;
--- src/components/navigation-bar/navigation-bar.styles.tsx
+++ src/components/navigation-bar/navigation-bar.styles.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { Link } from "react-router-dom";
+
 export const NavigationBarContainer = styled.div`
     align-items: center;
     background-color: rgba(0, 0, 255, 0.5);
@@ -9,17 +11,24 @@ export const NavigationBarContainer = styled.div`
     padding-left: 10px;
     padding-right: 20px;
     position: fixed;
+    top: -50px;
+    transition: top 0.3s;
     width: 100vw;
     z-index: 2;
+
+    &.active {
+        top: 0px;
+    }
 `;
 
-const BaseLogoContainer = styled.div`
+const BaseLogoContainer = styled(Link)`
     align-items: center;
-    cursor: default;
+    color: #fff;
     display: flex;
     font-size: 2rem;
     font-style: italic;
     font-weight: bold;
+    text-decoration: none;
     user-select: none;
     -ms-user-select: none;
     -webkit-user-select: none;
@@ -47,9 +56,13 @@ export const NavigationBarLinksContainer = styled.nav`
         color: #fff;
         text-decoration: none;
 
-        &:hover {
+        &:hover, &:active {
             color: #ebebeb;
             text-decoration: underline #ebebeb;
+        }
+
+        &:active {
+            transform: translateY(0.1rem);
         }
     }
 
